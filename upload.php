c965de8ac74a0e1f61e123909fc9c72a2ce68512
lap-a-toon <lap-a-toon@yandex.ru>
2024-03-12 23:43:44 +0500

    й

--- upload.php
+++ upload.php
@@ -10,8 +10,8 @@ if (!file_exists($uploadDir)) {
     mkdir($uploadDir, 0777, true);
 }
 if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadFile)) {
-    echo "Файл успешно загружен:\n";
-    echo $uploadFile;
+    echo "Файл успешно загружен:<br>$uploadFile<br>";
+    echo "Размер: " . $_FILES['content']['size'] . " bytes";
 } else {
     echo "Что-то пошло не так";
 }
